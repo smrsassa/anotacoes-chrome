a0e1d280a4cf37fcb9b8bf93ddf7e8ad8d7b0ece
Samuel Mayer <samrufino13@gmail.com>
2021-07-21 14:49:57 -0300

    radios funcionado

--- src/js/config.js
+++ src/js/config.js
@@ -4,16 +4,63 @@ function reloadVarsConfig() {
     const elemSwitchAsk = $("#switchAsk")
     const elemSwitchCredt = $("#switchCredt")
 
+    var config = {
+        Tema: "checked",
+        ClearAll: "checked",
+        Ask: "",
+        Credt: "checked"
+    }
+
+    function storageConfig() {
+        chrome.storage.local.set({ "anotacoesConfig": config }, () => {})
+    }
+
+    function carregarAnotacoes() {
+        chrome.storage.local.get("anotacoesConfig", (items) => {
+            let source = items.anotacoesConfig
+            Object.assign(config, source)
+            storageConfig()
+            inputState()
+        })
+    }
+
+    function inputState() {
+        elemSwitchTema.prop("checked", config.Tema === "checked")
+        elemSwitchClearAll.prop("checked", config.ClearAll === "checked")
+        elemSwitchAsk.prop("checked", config.Ask === "checked")
+        elemSwitchCredt.prop("checked", config.Credt === "checked")
+    }
+
+    function switchChecked( param ) {
+        return param === "checked" ? "" : "checked"
+    }
+
+    function saveSwitch(params) {
+        Object.assign(config, params)
+        storageConfig()
+        inputState()
+    }
+
     elemSwitchTema.on("change", () => {
-        alert("tema")
+        saveSwitch({
+            Tema: switchChecked(config.Tema)
+        })
     })
     elemSwitchClearAll.on("change", () => {
-        alert("clear")
+        saveSwitch({
+            ClearAll: switchChecked(config.ClearAll)
+        })
     })
     elemSwitchAsk.on("change", () => {
-        alert("ask")
+        saveSwitch({
+            Ask: switchChecked(config.Ask)
+        })
     })
     elemSwitchCredt.on("change", () => {
-        alert("credt")
+        saveSwitch({
+            Credt: switchChecked(config.Credt)
+        })
     })
+
+    carregarAnotacoes()
 }
